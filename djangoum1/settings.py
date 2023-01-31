a0543830f4b0966a7f6a4112ffca65f339e93ebb
Vitor Angheben <vitor_angheben@hotmail.com>
2023-01-30 23:02:01 -0300

    Login HTML page created with bootstrap url created

--- djangoum1/settings.py
+++ djangoum1/settings.py
@@ -125,6 +125,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- djangoum1/urls.py
+++ djangoum1/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name='index.html'), name='index'),
 ]
 
 admin.site.site_header = "Login Page"
